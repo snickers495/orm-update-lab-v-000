1ae6262b0a842a73bfc151271cbe2097da79dfad
Lisa Lee <snickers495@gmail.com>
2017-10-21 22:43:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
+    student = self.new(row[1], row[2])
     student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
   end
 
   def self.find_by_name
