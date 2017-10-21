70c13ee4274784c620ac9843b12297845fed9b96
Lisa Lee <snickers495@gmail.com>
2017-10-21 22:28:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,38 +4,38 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize 
+  def initialize
 
-  end 
+  end
 
   def self.create_table
 
-  end 
+  end
 
   def self.drop_table
 
-  end 
+  end
 
   def save
-  end 
+  end
 
-  def self.create 
+  def self.create
 
-  end 
+  end
 
   def self.new_from_db
 
-  end 
+  end
 
   def self.find_by_name
 
-  end 
+  end
 
-  def update 
+  def update
 
-  end 
+  end
 
-  
+
 
 
 end
